15cab23417d75a6d435959a57730bddd8caf32d2
dlucasmoura <lucasdsmoura@hotmail.com>
2022-09-22 14:59:13 -0300

    Resolvendo primeira etapa do exercicio-01 de JS, cinco programas de aritmetica basica

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-1-introcucao-a-javascript/exercicio-1.js
@@ -0,0 +1,13 @@
+let a = 20;
+let b = 10;
+
+console.log("O resultado da soma é: " + (a + b));
+
+console.log("O resultado da subtração é: " + (a - b));
+
+console.log("O resultado da Multiplicação é: " + (a * b));
+
+console.log("O resultado da Divisão é: " + (a / b));
+
+console.log("O resultado do Módulo é: " + (a % b));
+
